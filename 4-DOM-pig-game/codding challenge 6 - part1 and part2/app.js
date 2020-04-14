ce8dbe9d70162cf2f67096a0ae1296442ce1e932
luizfverissimo <luizfverissimo@gmail.com>
2020-04-13 22:45:37 -0300

    codding challenge 6

--- 4-DOM-pig-game/codding challenge 6 - part1 and part2/app.js
+++ 4-DOM-pig-game/codding challenge 6 - part1 and part2/app.js
@@ -8,12 +8,12 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, gamePlaying;
-
+var scores, roundScore, activePlayer, gamePlaying, scoreMax, sixCount, dice;
 
 init();
 function init() {
-  gamePlaying = true
+  sixCount = 0;
+  gamePlaying = true;
   scores = [0, 0];
   roundScore = 0;
   activePlayer = 0; //player 01 = 0, player 2 = 1
@@ -38,21 +38,28 @@ function init() {
 document.querySelector(".btn-roll").addEventListener("click", function () {
   if (gamePlaying) {
     //Gera um número randômico entre 1 a 6
-    var dice = Math.floor(Math.random() * 6) + 1;
-
+    dice = Math.floor(Math.random() * 6) + 1;
+    sixVerify(); //chama a contagem cada vez que apertamos o botão
     //Mostra o resultado:
     //adiciona esse texto em uma variável para não ficar escrevendo
     var diceDom = document.querySelector(".dice");
     diceDom.style.display = "block"; //block faz aparecer a imagem
     diceDom.src = `dice-${dice}.png`;
+    //Define o score máximo
+    scoreMax = document.getElementById("inp-number").value;
 
     //Atualiza o round score somente IF o valor não for 1
-    if (dice !== 1) {
+    if (dice !== 1 && sixCount !== 2) {
       //add score
       roundScore += dice;
       document.querySelector(
         `#current-${activePlayer}`
       ).textContent = roundScore;
+    } else if (sixCount === 6) {
+      //se a contagem for 2, quer dizer que dois 6 sairam em seguinda, logo zera o score total
+      scores[activePlayer] = 0;
+      document.querySelector("#score-" + activePlayer).textContent = 0;
+      nextPlayer();
     } else {
       //Next player
       nextPlayer();
@@ -70,7 +77,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
       scores[activePlayer];
 
     //check fi player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= scoreMax) {
       document.querySelector("#name-" + activePlayer).textContent = "WINNER!";
       document.querySelector(".dice").style.display = "none";
       document
@@ -87,6 +94,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
 });
 
 function nextPlayer() {
+  sixCount = 0;
   //Next player
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0); //operador ternário
   roundScore = 0; //reseta para Zero o current
@@ -98,3 +106,12 @@ function nextPlayer() {
 }
 
 document.querySelector(".btn-new").addEventListener("click", init);
+
+//Verifica se os 6 sairam seguidos, se sairam sixCount ficará 2
+function sixVerify() {
+  if (dice === 3) {
+    sixCount++;
+  } else {
+    sixCount = 0;
+  }
+}
