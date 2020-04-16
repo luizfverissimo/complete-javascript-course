f28978dc4ccb3fecb9e85cd18da43d69f208689f
luizfverissimo <luizfverissimo@gmail.com>
2020-04-15 23:04:23 -0300

    Update script.js

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,5 +1,5 @@
 // Function constructor
-
+/*
 var john = {
   name: "John",
   yearOfBirth: 1990,
@@ -47,3 +47,102 @@ var jane1 = new RuningDistante('Jane', 42, 4)
 john1.velocity()
 mark1.velocity()
 jane1.velocity()
+*/
+
+//Object.creat
+/*
+var personProto = {
+  calculateAge: function(){
+    console.log(2020 - this.yearOfBirth)
+  }
+}
+
+var john = Object.create(personProto)
+john.name = 'John'
+john.yearOfBirth = 1990
+john.job = 'teacher'
+
+var jane = Object.create(personProto,
+  {
+    name: {value: 'Jane'},
+    yearOfBirth: {valeu: 1969},
+    job: {value: 'designer'}
+
+})
+*/
+/*
+//Primitives vs Objetcts
+
+//Primitives
+// variáveis guardando primitivos, a variável b guardou uma cópia de a(23)
+var a = 23
+var b = a
+a = 46
+console.log(a)//mostra 46
+console.log(b)//mostra 23
+
+//objects
+var obj1 = {
+  name: 'John',
+  age: 26
+}
+
+var obj2 = obj1 //ambos guarda uma referência que apontam para o mesmo local da memória
+obj1.age = 30
+console.log(obj1.age)// mostra 30
+console.log(obj2.age)// mostra 30
+
+//Functions
+
+var age = 26
+var obj = {
+  name: 'Jonas',
+  city: 'Lisbon'
+}
+
+function change(a, b){
+  a = 30
+  b.city = 'San Francisco'
+}
+
+change(age, obj)
+console.log(age)
+console.log(obj.city)
+*/
+
+//Functions: passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2020 - el;
+}
+
+function isFullAge(el) {
+  return el >= 19;
+}
+
+function MaxHR(el){
+  if (el >= 19 && el <= 81){
+    return Math.round(206.9 - (0.67 * el))
+  } else {
+    return -1
+  }
+  
+}
+
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge)
+var rates = arrayCalc(ages, MaxHR)
+
+console.log(ages);
+console.log(fullAges)
+console.log(rates)
