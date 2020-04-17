02cce0466635d9d361579886b64a0298a92ba5db
luizfverissimo <luizfverissimo@gmail.com>
2020-04-16 23:10:43 -0300

    Update script.js

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -248,32 +248,36 @@ interviewQuestion('designer')('John')
 // Bind, Call and Apply
 
 var john = {
-  name: 'John',
+  name: "John",
   age: 26,
-  job: 'teacher',
-  presentation: function(style, timeOfDay) {
-    if (style === 'formal'){
-      console.log(`Good ${timeOfDay}, Ladies and gentlemen! I'm ${this.name}, I'm a ${this.job} and I'm ${this.age} years old.`)
-    } else if (style === 'friendly'){
-      console.log(`Hey! What's up? I'm ${this.name}, I'm a ${this.job} and I'm ${this.age} years old. Have a nice ${timeOfDay}`)
+  job: "teacher",
+  presentation: function (style, timeOfDay) {
+    if (style === "formal") {
+      console.log(
+        `Good ${timeOfDay}, Ladies and gentlemen! I'm ${this.name}, I'm a ${this.job} and I'm ${this.age} years old.`
+      );
+    } else if (style === "friendly") {
+      console.log(
+        `Hey! What's up? I'm ${this.name}, I'm a ${this.job} and I'm ${this.age} years old. Have a nice ${timeOfDay}`
+      );
     }
-  }
-}
+  },
+};
 
 var emily = {
-  name: 'Emily',
+  name: "Emily",
   age: 35,
-  job: 'designer'
-}
+  job: "designer",
+};
 
-john.presentation('formal', 'morning')
-john.presentation.call(emily, 'friendly', 'afternoon')
+john.presentation("formal", "morning");
+john.presentation.call(emily, "friendly", "afternoon");
 
 //john.presentation.apply(emily, ['friendly', 'afternoon'])
 
-var johnFriendly = john.presentation.bind(john, 'friendly')
-johnFriendly('morning')
-johnFriendly('night')
+var johnFriendly = john.presentation.bind(john, "friendly");
+johnFriendly("morning");
+johnFriendly("night");
 
-var emilyFormal = john.presentation.bind(emily, 'formal')
-emilyFormal('afternoon')
+var emilyFormal = john.presentation.bind(emily, "formal");
+emilyFormal("afternoon");
