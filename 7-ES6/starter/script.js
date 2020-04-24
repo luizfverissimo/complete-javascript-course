a39f89c803487150ed6987f521e04d75298610cc
luizfverissimo <luizfverissimo@gmail.com>
2020-04-23 21:28:08 -0300

    Update script.js

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -129,7 +129,7 @@ console.log(ages6)
 
 //////////////////////////////////////////////////////////////////////////////
 //Arrow function - lexical this keyword
-
+/*
 //ES5
 var box5 = {
     color: 'green',
@@ -157,6 +157,7 @@ const box6 = {
     }
 }
 //box6.clickme()
+*/
 /*
 const box66 = {
     color: 'green',
@@ -170,7 +171,7 @@ const box66 = {
 }
 box66.clickme()
 */
-
+/*
 //ES5
 function Person(name){
     this.name = name
@@ -195,3 +196,210 @@ Person.prototype.myFriends6 = function(friends){
 }
 
 new Person('Mike').myFriends6(friends)
+*/
+
+//////////////////////////////////////////////////////////////////////////////
+//Destrucuring
+
+//ES5
+/*
+var john = ['John', 26]
+var name = john[0]
+var age = john[1]
+*/
+/*
+//ES6
+const [name, age] = ['John', 26]
+console.log(name)
+console.log(age)
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+}
+
+const {firstName, lastName} = obj
+console.log(firstName)
+console.log(lastName)
+
+const {firstName: a, lastName: b} = obj
+console.log(a)
+console.log(b)
+
+function calcAgeRetirement(year){
+    const age = new Date().getFullYear() - year
+    return [age, 65 - age]
+}
+
+const [age2, retirement] = calcAgeRetirement(1990)
+console.log(age2)
+console.log(retirement)
+*/
+
+//////////////////////////////////////////////////////////////////////////////
+//Arrays
+/*
+const boxes = document.querySelectorAll('.box')
+*/
+//ES5
+/*
+var boxesArr5 = Array.prototype.slice.call(boxes)
+
+boxesArr5.forEach(function(cur){
+    cur.style.backgroundColor = 'dodgerblue'
+})
+*/
+//ES6
+/*
+const boxesArr6 = Array.from(boxes)
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue')
+*/
+/*
+//ES5
+for(var i = 0; i < boxesArr5.length; i++){
+    if(boxesArr5[i].className === 'box blue'){
+        continue        
+    }
+
+    boxesArr5[i].textContent = 'I changed to blue'
+}
+*/
+
+//ES6
+/*
+for(const cur of boxesArr6) {
+    if(cur.className.includes('blue')) {
+        continue
+    }
+    cur.textContent = 'I changed to blue'
+}
+*/
+//find elements in an Array
+//ES5
+/*
+var ages = [12, 17, 8, 21, 14, 11]
+
+var full = ages.map(function(cur){
+    return cur >= 18
+})
+console.log(full)
+
+console.log(full.indexOf(true))
+console.log(ages[full.indexOf(true)])
+
+//ES6
+console.log(ages.findIndex(cur => cur >= 18))
+console.log(ages.find(cur => cur >= 18))
+*/
+
+/////////////////////////////////////////////////////////////////////////////
+//Spread operator
+/*
+function addFourAges (a, b, c, d){
+    return a + b + c + d
+}
+
+var sum1 = addFourAges(18, 30, 12, 21)
+console.log(sum1)
+
+//ES5
+var ages = [18, 30, 12, 21]
+var sum2 = addFourAges.apply(null, ages)
+
+console.log(sum2)
+
+//ES6
+const sum3 = addFourAges(...ages)
+console.log(sum3)
+
+const familySmith = ['John', 'Jane', 'Mark']
+const familyMiller = ['Mary', 'Bob', 'Ann']
+const bigFamily = [...familySmith, 'Lily', ...familyMiller]
+
+console.log(bigFamily)
+
+const h = document.querySelector('h1')
+const boxes = document.querySelectorAll('.box')
+
+const all = [h, ...boxes]
+Array.from(all).forEach(cur => cur.style.color = 'purple')
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+//Rest parameters - function
+/*
+//ES5
+function isFullAge5(){
+    //console.log(arguments)
+    var argsArr = Array.prototype.slice.call(arguments)
+    
+    argsArr.forEach(function(cur){
+        console.log((2020 - cur) >= 18)
+    })
+}
+
+//isFullAge5(1990, 2005, 1965)
+//isFullAge5(1990, 2005, 1965, 2016, 1987)
+
+//ES6
+function isFullAge6(...years){
+    years.forEach(cur => console.log((2020 - cur) >= 18))
+    
+}
+
+//isFullAge6(1990, 2005, 1965)
+isFullAge6(1990, 2005, 1965, 2016, 1987)
+*/
+/*
+//ES5
+function isFullAge5(limit){
+    //console.log(arguments)
+    var argsArr = Array.prototype.slice.call(arguments, 1)
+    //console.log(argsArr)
+    argsArr.forEach(function(cur){
+        console.log((2020 - cur) >= limit)
+    })
+}
+
+//isFullAge5(16, 1990, 2005, 1965)
+//isFullAge5(1990, 2005, 1965, 2016, 1987)
+
+//ES6
+function isFullAge6(limit, ...years){
+    years.forEach(cur => console.log((2020 - cur) >= limit))
+    
+}
+
+//isFullAge6(1990, 2005, 1965)
+isFullAge6(16, 1990, 2005, 1965, 2016, 1987)
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+//Defaut parameters
+/*
+function SMithPerson(firstName, yearOfBrith, lastName, nationality) {
+  lastName === undefined ? lastName = "Smith" : lastName = lastName;
+  nationality === undefined ? nationality = 'american' : nationality = nationality
+
+  this.firstName = firstName;
+  this.yearOfBrith = yearOfBrith;
+  this.lastName = lastName;
+  this.nationality = nationality;
+}
+
+var john = new SMithPerson("John", 1990);
+
+var emily = new SMithPerson('Emily', 1983, 'Diaz', 'spanish')
+*/
+
+//ES6
+function SmithPerson (firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
+    this.firstName = firstName;
+    this.yearOfBirth = yearOfBirth;
+    this.lastName = lastName;
+    this.nationality = nationality;
+}
+
+var john = new SmithPerson("John", 1990);
+
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish')
