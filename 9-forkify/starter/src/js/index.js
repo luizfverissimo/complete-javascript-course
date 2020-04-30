89a91c2fc94d38abf4dcc5821d65805a6f0d9209
luizfverissimo <luizfverissimo@gmail.com>
2020-04-30 20:11:15 -0300

    search controller

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,37 @@
-// Global app controller
-import num from "./test";
-const x = 23
-console.log(`I imported ${num} from another module called test.js Variable x is ${x}!`);
+import Search from "./models/Search"
+
+/*Global state of the app
+    * - Search object
+    * - Current recipe object
+    * - Shopping list object
+    * - Liked recipes
+ */
+const state = {}
+
+const controlSearch = async () => {
+    //get a query from view
+    const query = 'pizza' //TODO
+
+    if(query){
+        //create new search obj
+        state.search = new Search(query)
+
+        //Prepare UI for results
+
+        //Search for recepies
+        await state.search.getResults()
+
+        //Render result on UI
+        console.log(state.search.result)
+    }
+
+    
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault()
+    controlSearch()
+})
+
+
+search.getResults()
